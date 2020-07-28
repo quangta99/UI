b67553b21d9c63947b899e03ca4a131b51c3c0d4
quangta99 <txqsevena@gmail.com>
2020-07-29 00:50:26 +0700

    update

--- server.js
+++ server.js
@@ -1,24 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
+
 const app = express();
- 
-const forceSSL = function () {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-        ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-};
-app.use(express.static('./dist/cpharma'));
- 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname,'/dist/cpharma/index.html'));
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/cpharma'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/cpharma/index.html'));
 });
- 
-app.use(forceSSL());
- 
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
